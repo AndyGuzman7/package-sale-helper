7ea709bc8005eb0eebfe47bab3a14cf4c250e299
andyguzman7 <andyguzman117gmail.com>
2024-09-13 18:51:52 -0400

    add: order number

---
+++ lib/order_number/generate_order_number.dart
@@ -0,0 +1,14 @@
+class GenerateOrderInvoice {
+  int getOrderNumber(int idOrder, int lastIdOrder) {
+    final lastIdIsGreater = lastIdOrder > idOrder;
+
+    var idDocument = 0;
+
+    if (lastIdIsGreater) {
+      idDocument = lastIdOrder + 1;
+    } else {
+      idDocument = idOrder + 1;
+    }
+    return idDocument;
+  }
+}
--- lib/sale_helper_zencillo.dart
+++ lib/sale_helper_zencillo.dart
@@ -3,3 +3,4 @@ library sale_helper_zencillo;
 export 'cufe/cufe.dart';
 export 'document_number/document_number.dart';
 export 'invoice_number/invoice_number.dart';
+export 'order_number/order_number.dart';
