e9c006a690b063d80bb390ddbac8841dcb0d0f51
andyguzman7 <andyguzman117gmail.com>
2024-09-17 13:06:24 -0400

    comment to handlre

--- lib/sale_helper_zencillo.dart
+++ lib/sale_helper_zencillo.dart
@@ -4,4 +4,4 @@ export 'cufe/cufe.dart';
 export 'document_number/document_number.dart';
 export 'invoice_number/invoice_number.dart';
 export 'order_number/order_number.dart';
-export 'process_interaction/process_interaction.dart';
+//export 'process_interaction/process_interaction.dart';
